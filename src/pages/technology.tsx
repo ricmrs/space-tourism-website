a89072ee0aa858afee9206201a62deed4ec352f1
Ricardo <83619661+ricmrs@users.noreply.github.com>
2023-03-25 16:03:40 -0300

    Change  to SSR

--- src/pages/technology.tsx
+++ src/pages/technology.tsx
@@ -2,7 +2,7 @@ import { loadTechnologys } from '@/lib/Technology';
 
 export { default } from '@/screens/TechnologyPage';
 
-export async function getStaticProps() {
+export async function getServerSideProps() {
   const technologys = await loadTechnologys();
   return {
     props: {
